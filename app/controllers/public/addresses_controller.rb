ce9a5c9cec319493d57418d9b0a697cf7142642a
Emika Kiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-02-17 11:53:28 +0000

    [配送先登録機能を実装しました]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,24 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+  
+  def create
+    @address = address.new(address_params)
+    @address.member_id = current_member.id
+    @address.save
+    redirect_to request.referer
   end
 
   def edit
+  end
+
+
+  private
+  
+  def address_params
+    params.require(:address).permit(:address, :postal_code, :shipping_name)
+    # 住所、郵便番号、宛名の保存を許可しています。
   end
 end
