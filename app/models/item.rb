cad66935d306963f7a99104afc3e4c904fd890cd
EC2 Default User <ec2-user@ip-172-31-11-216.ap-northeast-1.compute.internal>
2023-02-17 12:52:18 +0000

    [商品新規登録機能の実装]

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  has_many :cart_items
+  has_many :order_items
+  has_one_attached :image
+  
+  with_options presence: true do
+   validates :name
+   validates :introduction
+   validates :price
+   validates :is_active
+   validates :image
+ end
 end
