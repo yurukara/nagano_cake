42c50afb98a22bafb5e0e8912bcfd5708620ef17
KoketsuRyuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-02-17 07:25:35 +0000

    [Finish]ジャンル機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,8 +3,21 @@ class Admin::GenresController < ApplicationController
     @genres = Genre.all
     @genre = Genre.new
   end
+  
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to request.referer
+  end
 
   def edit
-    
+    @genre = Genre.find(params[:id])
   end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
 end
