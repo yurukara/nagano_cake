8faf3ec2bc67f326f965b2aa26d7421f9ab54540
KoketsuRyuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-02-17 14:20:01 +0000

    [admin会員一覧作成]

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,10 +1,15 @@
 class Admin::MembersController < ApplicationController
   def index
+    @members = Members.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def update
+    
   end
 end
