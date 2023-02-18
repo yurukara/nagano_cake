77fca1552e265d612220c95a1b07a9ad090101dd
KoketsuRyuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-02-18 07:34:05 +0000

    [Finish]顧客編集機能

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -8,10 +8,18 @@ class Admin::MembersController < ApplicationController
   end
 
   def edit
-
+    @member = Member.find(params[:id])
   end
 
   def update
-
+    @member = Member.find(params[:id])
+    @member.update(member_params)
+    redirect_to admin_member_path(@member)
+  end
+  
+  private
+  
+  def member_params
+    params.require(:member).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:phone_number,:email)
   end
 end
