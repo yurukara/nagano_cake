393b443001175be5651c825f3aa56d61b8da8794
Emika Kiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-02-18 10:31:41 +0000

    [商品一覧・詳細画面作成。商品をカートに追加する機能追加しました]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,27 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_member.cart_items
   end
+  
+  def create
+    @cart_item = current_member.cart_items.new(cart_item_params)
+    if current_member.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_member.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.quantity += params[:cart_item][:quantity].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+          @cart_items = current_member.cart_items
+          render 'index'
+    else
+          render 'index'
+    end
+  end
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :member_id, :quantity)
+  end
+  
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,10 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :cart_items
+  has_many :addresses
+
 
   validates :last_name, presence:true
   validates :first_name, presence:true
