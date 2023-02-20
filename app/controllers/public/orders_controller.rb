5b2eb38f10de4524d3b3e956eaf2063580725191
Emika Kiyohara <119833213+kiyo44hara@users.noreply.github.com>
2023-02-20 10:35:00 +0000

    [注文一覧画面9割完了しました]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -56,7 +56,8 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-    
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
   
   private
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,6 @@ class Order < ApplicationRecord
   
   enum payment_method: ["クレジットカード", "銀行振込"]
   
+  enum order_status: ["入金待ち","入金確認","制作中","発送準備中","発送済み"]
+  
 end
