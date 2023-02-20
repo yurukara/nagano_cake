cb0c428f798379a35fa3d0764dd36293b7773ef6
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-02-20 04:43:27 +0000

    [Add]新規登録画面及びログイン画面のレイアウト調整

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,11 @@
 class Public::CartItemsController < ApplicationController
-
+  before_action :authenticate_member!
 
   def index
     @cart_items = current_member.cart_items
     @total = @cart_items.inject(0) {|sum, item| sum + item.subtotal}
   end
-  
+
   def create
     @cart_item = current_member.cart_items.new(cart_item_params)
     if current_member.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
@@ -44,5 +44,5 @@ class Public::CartItemsController < ApplicationController
   def cart_item_params
     params.require(:cart_item).permit(:quantity,:member_id,:item_id)
   end
-  
+
 end
