cbedaff814a19741ffa62022e480ed0d4cc56fd1
Koketsu Ryuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-02-21 14:03:22 +0900

    Update order.rb
    注文ステータスのenumを削除しました

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,14 +5,6 @@ class Order < ApplicationRecord
 
   enum payment_method: ["クレジットカード", "銀行振込"]
 
-  enum order_status: {
-    "入金待ち" => 0,
-    "入金確認" => 1,
-    "製作中" => 2,
-    "発送準備中" => 3,
-    "発送済み" => 4,
-  }
-
   def set_date
     created_at.strftime("%Y/%-m/%-d/ %-H:%M:%S")
   end
