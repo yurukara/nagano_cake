18e5d0137474cc97587bb41d07b77a48175dd593
KoketsuRyuto <120470266+KoketsuRyuto@users.noreply.github.com>
2023-02-22 01:34:54 +0000

    [Finish]ジャンルデザイン修正

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,6 +1,6 @@
 class Admin::MembersController < ApplicationController
   def index
-    @members = Member.page(params[:page]).per(4)
+    @members = Member.page(params[:page]).per(10)
   end
 
   def show
